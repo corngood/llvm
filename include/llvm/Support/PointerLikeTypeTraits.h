corngood/llvm

commit 91d160ca8a93104908bbeca45f840ae3da897963

teach PointerLikeTypeTraits that all pointers to pointers may only be 4-byte aligned.
git-svn-id: http://llvm.org/svn/llvm-project/llvm/trunk@68147 91177308-0d34-0410-b5e6-96231b3b80d8

include/llvm/Support/PointerLikeTypeTraits.h
@@ -58,6 +58,20 @@ public:
   enum { NumLowBitsAvailable = 3 };
 };
 
+// Pointers to pointers are only 4-byte aligned on 32-bit systems.
+template<typename T>
+class PointerLikeTypeTraits<T**> {
+public:
+  static inline void *getAsVoidPointer(T** P) { return P; }
+  static inline T **getFromVoidPointer(void *P) {
+    return static_cast<T**>(P);
+  }
+  enum { NumLowBitsAvailable = 2 };
+};
+  
+  
+  
+  
 // Provide PointerLikeTypeTraits for uintptr_t.
 template<>
 class PointerLikeTypeTraits<uintptr_t> {

include/llvm/Use.h
@@ -29,17 +29,6 @@ class Use;
 /// Tag - generic tag type for (at least 32 bit) pointers
 enum Tag { noTag, tagOne, tagTwo, tagThree };
 
-// Use** is only 4-byte aligned.
-template<>
-class PointerLikeTypeTraits<Use**> {
-public:
-  static inline void *getAsVoidPointer(Use** P) { return P; }
-  static inline Use **getFromVoidPointer(void *P) {
-    return static_cast<Use**>(P);
-  }
-  enum { NumLowBitsAvailable = 2 };
-};
-
 //===----------------------------------------------------------------------===//
 //                                  Use Class
 //===----------------------------------------------------------------------===//

commit 0b89a2d564907be1fdf547e2aa13c896ac76e5a1

Work around an apparent GCC miscompilation by specializing different, this fixes a regression on some compilers from r68147.
git-svn-id: http://llvm.org/svn/llvm-project/llvm/trunk@68356 91177308-0d34-0410-b5e6-96231b3b80d8

include/llvm/Support/PointerLikeTypeTraits.h
@@ -58,20 +58,6 @@ public:
   enum { NumLowBitsAvailable = 3 };
 };
 
-// Pointers to pointers are only 4-byte aligned on 32-bit systems.
-template<typename T>
-class PointerLikeTypeTraits<T**> {
-public:
-  static inline void *getAsVoidPointer(T** P) { return P; }
-  static inline T **getFromVoidPointer(void *P) {
-    return static_cast<T**>(P);
-  }
-  enum { NumLowBitsAvailable = 2 };
-};
-  
-  
-  
-  
 // Provide PointerLikeTypeTraits for uintptr_t.
 template<>
 class PointerLikeTypeTraits<uintptr_t> {

include/llvm/Support/ValueHandle.h
@@ -18,6 +18,18 @@
 #include "llvm/Value.h"
 
 namespace llvm {
+class ValueHandleBase;
+
+// ValueHandleBase** is only 4-byte aligned.
+template<>
+class PointerLikeTypeTraits<ValueHandleBase**> {
+public:
+  static inline void *getAsVoidPointer(ValueHandleBase** P) { return P; }
+  static inline ValueHandleBase **getFromVoidPointer(void *P) {
+    return static_cast<ValueHandleBase**>(P);
+  }
+  enum { NumLowBitsAvailable = 2 };
+};
 
 /// ValueHandleBase - This is the common base class of value handles.
 /// ValueHandle's are smart pointers to Value's that have special behavior when

include/llvm/Use.h
@@ -29,6 +29,17 @@ class Use;
 /// Tag - generic tag type for (at least 32 bit) pointers
 enum Tag { noTag, tagOne, tagTwo, tagThree };
 
+// Use** is only 4-byte aligned.
+template<>
+class PointerLikeTypeTraits<Use**> {
+public:
+  static inline void *getAsVoidPointer(Use** P) { return P; }
+  static inline Use **getFromVoidPointer(void *P) {
+    return static_cast<Use**>(P);
+  }
+  enum { NumLowBitsAvailable = 2 };
+};
+
 //===----------------------------------------------------------------------===//
 //                                  Use Class
 //===----------------------------------------------------------------------===//